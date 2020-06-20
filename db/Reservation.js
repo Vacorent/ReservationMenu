22ed55a9a463f83f556405e2d37070d5a602f64a
Watson Fu <watsonfu18@gmail.com>
2020-06-19 23:42:42 -0500

    Added linter and nodemon

--- db/Reservation.js
+++ db/Reservation.js
@@ -9,22 +9,20 @@ const calendarDateSchema = new mongoose.Schema({
   cost: Number,
   costCurrency: String,
 },
-  {
-  timestamps: false
-  }
-);
+{
+  timestamps: false,
+});
 
 const resDataSchema = new mongoose.Schema({
   _id: Number,
   reviewAverage: Number,
   reviewCount: Number,
   guestCapacity: Number,
-  calendar: [calendarDateSchema]
+  calendar: [calendarDateSchema],
 },
-  {
-    timestamps: false
-  }
-);
+{
+  timestamps: false,
+});
 
 const Reservation = mongoose.model('reservations', resDataSchema);
 
