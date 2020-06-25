a6fd39ee6b990449e1840b7a749e7fc33f458552
Watson Fu <watsonfu18@gmail.com>
2020-06-25 11:23:44 -0500

    Converted month to class component

--- client/src/components/Month.jsx
+++ client/src/components/Month.jsx
@@ -2,41 +2,76 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'moment';
 
+class Month extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-
-const Month = ({ data, monthNum, yearNum }) => {
-  const formattedMonth = Moment(monthNum, 'M').format('MMMM');
-
-  return (
-    <>
-    <div>
-      {formattedMonth}
-      {' '}
-      {yearNum}
-    <table>
-      <thead>
-        <tr>
-          <th>Su</th>
-          <th>Mo</th>
-          <th>Tu</th>
-          <th>We</th>
-          <th>Th</th>
-          <th>Fr</th>
-          <th>Sa</th>
-        </tr>
-      </thead>
-      <tbody>
-      </tbody>
-      </table>
-    </div>
-    </>
-  )
+  render() {
+    const { data, monthNum, yearNum } = this.props;
+    const formattedMonth = Moment(monthNum, 'M').format('MMMM');
+    return (
+      <>
+        <div>
+          {formattedMonth}
+          {' '}
+          {yearNum}
+          <table>
+            <thead>
+              <tr>
+                <th>Su</th>
+                <th>Mo</th>
+                <th>Tu</th>
+                <th>We</th>
+                <th>Th</th>
+                <th>Fr</th>
+                <th>Sa</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
+      </>
+    )
+  }
 }
 
-Month.propTypes = {
-  data: PropTypes.object.isRequired,
-  monthNum: PropTypes.number.isRequired,
-  yearNum: PropTypes.number.isRequired,
-};
+// const Month = ({ data, monthNum, yearNum }) => {
+//   const formattedMonth = Moment(monthNum, 'M').format('MMMM');
+
+//   return (
+//     <>
+//     <div>
+//       {formattedMonth}
+//       {' '}
+//       {yearNum}
+//     <table>
+//       <thead>
+//         <tr>
+//           <th>Su</th>
+//           <th>Mo</th>
+//           <th>Tu</th>
+//           <th>We</th>
+//           <th>Th</th>
+//           <th>Fr</th>
+//           <th>Sa</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//       </tbody>
+//       </table>
+//     </div>
+//     </>
+//   )
+// }
+
+// Month.propTypes = {
+//   data: PropTypes.object.isRequired,
+//   monthNum: PropTypes.number.isRequired,
+//   yearNum: PropTypes.number.isRequired,
+// };
 
 export default Month;
