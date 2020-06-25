8db2531675b04fa24fd645a836779391557a3c96
Watson Fu <watsonfu18@gmail.com>
2020-06-25 13:43:14 -0500

    Created working calendar scrolling with correct dates ITS LIT FAM

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -9,14 +9,14 @@ class Calendar extends React.Component {
     super(props);
     this.state = {
       currentMonth: Moment().month(),
-      nextMonth: Moment().month() + 1,
       currentMonthYear: Moment().year(),
-      nextMonthYear: Moment().add(1, 'months').year(),
       startDate: '',
       endDate: '',
       isDropdown: false,
     };
     this.dropdownClick = this.dropdownClick.bind(this);
+    this.prevMonthClick = this.prevMonthClick.bind(this);
+    this.nextMonthClick = this.nextMonthClick.bind(this);
   }
 
   dropdownClick(e) {
@@ -28,25 +28,52 @@ class Calendar extends React.Component {
     e.preventDefault();
   }
 
+  prevMonthClick(e) {
+    console.log('prevmonth clicked');
+    const { currentMonth, currentMonthYear } = this.state;
+    this.setState({
+      currentMonth: currentMonth === 0 ? 11 : currentMonth - 1,
+      currentMonthYear: currentMonth === 0 ? currentMonthYear - 1 : currentMonthYear,
+    })
+    e.preventDefault();
+  }
+
+  nextMonthClick(e) {
+    console.log('nextmonth clicked');
+    const { currentMonth, currentMonthYear } = this.state;
+    this.setState({
+      currentMonth: currentMonth === 11 ? 0 : currentMonth + 1,
+      currentMonthYear: currentMonth === 11 ? currentMonthYear + 1 : currentMonthYear,
+    })
+    e.preventDefault();
+  }
+
   renderView() {
-    const { isDropdown, currentMonth, nextMonth, currentMonthYear, nextMonthYear } = this.state;
+    const { isDropdown, currentMonth, currentMonthYear } = this.state;
     const { calendar } = this.props;
+    const nextMonth = Moment([currentMonthYear, currentMonth]).add(1, 'months');
+    const nextMonthNum = nextMonth.month();
+    const nextMonthYear = nextMonth.year();
+    const textCurrMonth = Moment(currentMonth + 1, 'M').format('MMMM');
+    const textNextMonth = Moment(currentMonth + 2, 'M').format('MMMM');
     if (isDropdown) {
       return (
         <tbody className="dropdownCalendar" >
           <tr>
             <td>
-              <Month data={calendar[currentMonth]} monthNum={currentMonth} yearNum={currentMonthYear} />
+              <button onClick={this.prevMonthClick}>{'<'}</button><div>{textCurrMonth} {currentMonthYear}</div>
+                <Month data={calendar[currentMonth]} monthNum={currentMonth} yearNum={currentMonthYear} />
             </td>
-            <td>
-              <Month data={calendar[nextMonth]} monthNum={nextMonth} yearNum={nextMonthYear} />
-            </td>
+              <td>
+              <button onClick={this.nextMonthClick}>{'>'}</button><div>{textNextMonth} {nextMonthYear}</div>
+                <Month data={calendar[nextMonthNum]} monthNum={nextMonthNum} yearNum={nextMonthYear} />
+              </td>
           </tr>
-          <tr >
-            <td colSpan="2" className="calendarButtons">
-              <button>Clear dates</button>
-              <button>Close</button>
-            </td>
+            <tr >
+              <td colSpan="2" className="calendarButtons">
+                <button>Clear dates</button>
+                <button>Close</button>
+              </td>
             </tr>
         </tbody>
       )
@@ -55,37 +82,37 @@ class Calendar extends React.Component {
   }
 
   render() {
-    const { capacity } = this.props;
-    let { startDate, endDate } = this.state;
+    const { capacity} = this.props;
+    let { startDate, endDate} = this.state;
     if (!startDate) {
-      startDate = 'Add date';
+            startDate = 'Add date';
     }
     if (!endDate) {
-      endDate = 'Add date';
+            endDate = 'Add date';
     }
     return (
-      <table className="table">
-        <tbody>
-          <tr>
-            <td className="checkin" role="presentation" onClick={this.dropdownClick} onKeyDown={this.dropdownClick}>
-              <div className="staticText">CHECK-IN</div>
-              <div className="dynamicText">{startDate}</div>
-            </td>
-            <td className="checkout" role="presentation" onClick={this.dropdownClick} onKeyDown={this.dropdownClick}>
-              <div className="staticText">CHECKOUT</div>
-              <div className="dynamicText">{endDate}</div>
-            </td>
-          </tr>
-        </tbody>
-        {this.renderView()}
-        <Guests capacity={capacity} />
-      </table>
+          <table className="table">
+            <tbody>
+              <tr>
+                <td className="checkin" role="presentation" onClick={this.dropdownClick} onKeyDown={this.dropdownClick}>
+                  <div className="staticText">CHECK-IN</div>
+                  <div className="dynamicText">{startDate}</div>
+                </td>
+                <td className="checkout" role="presentation" onClick={this.dropdownClick} onKeyDown={this.dropdownClick}>
+                  <div className="staticText">CHECKOUT</div>
+                  <div className="dynamicText">{endDate}</div>
+                </td>
+              </tr>
+            </tbody>
+            {this.renderView()}
+            <Guests capacity={capacity} />
+          </table>
     );
   }
 }
 
 Calendar.propTypes = {
-  capacity: PropTypes.number.isRequired,
+            capacity: PropTypes.number.isRequired,
   calendar: PropTypes.object.isRequired,
 };
 
--- client/src/components/Month.jsx
+++ client/src/components/Month.jsx
@@ -10,18 +10,66 @@ class Month extends React.Component {
   }
 
   renderView() {
+    const { monthNum, yearNum } = this.props;
+    const startDayOfWeek = Moment([yearNum, monthNum]).startOf('month').day();
+    const daysInMonth = Moment([yearNum, monthNum]).daysInMonth();
+    let calRows = [];
+    let currRow = [];
+    let date = 1;
+    let dayCounter = startDayOfWeek;
+    while (date < daysInMonth + 1) {
+      if (date === 1 && dayCounter !== 0) {
+        for (let i = 0; i < dayCounter; i += 1) {
+          currRow.push('');
+        }
+        currRow.push(date);
+        if (dayCounter === 6) {
+          calRows.push(currRow);
+          currRow = [];
+          dayCounter = 0;
+        } else {
+          dayCounter += 1;
+        }
+      } else if (dayCounter === 6) {
+        currRow.push(date);
+        calRows.push(currRow);
+        currRow = [];
+        dayCounter = 0;
+      } else if (date === daysInMonth) {
+        currRow.push(date);
+        calRows.push(currRow);
+      } else {
+        currRow.push(date);
+        dayCounter += 1;
+      }
+      date += 1;
+    }
+
+    let calEntries = calRows.map((calRow, index) => {
+      let rowEntries = calRow.map((day, index) => {
+        return <td key={index}>{day}</td>
+      })
+      return (
+        <tr key={index}>
+          {rowEntries}
+        </tr>
+      )
+    })
+
+    return (
+      <>
+       {calEntries}
+      </>
+    )
 
   }
 
   render() {
     const { data, monthNum, yearNum } = this.props;
-    const formattedMonth = Moment(monthNum, 'M').format('MMMM');
+
     return (
       <>
         <div>
-          {formattedMonth}
-          {' '}
-          {yearNum}
           <table>
             <thead>
               <tr>
@@ -35,6 +83,7 @@ class Month extends React.Component {
               </tr>
             </thead>
             <tbody>
+              {this.renderView()}
             </tbody>
           </table>
         </div>
