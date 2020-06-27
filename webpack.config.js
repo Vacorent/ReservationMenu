5328d7e1663261d4b8586db8abdffe03b8b25e19
Watson Fu <watsonfu18@gmail.com>
2020-06-26 19:08:35 -0500

    Adjusted pointers

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   entry: `${__dirname}/client/src/index.jsx`,
-  resolve: {extensions: ['.js','.jsx']},
+  resolve: { extensions: ['.js', '.jsx'] },
   module: {
     rules: [
       {
