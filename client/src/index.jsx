8a02c7776f66d8a27ff2e115e69ffe889b374db4
Watson Fu <watsonfu18@gmail.com>
2020-06-28 18:06:29 -0500

    All basic functionality done

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -18,6 +18,9 @@ class App extends React.Component {
       calendar: {},
       startDate: '',
       endDate: '',
+      totalPrice: 0,
+      stayCal: [],
+      stayDuration: 0,
       adultCount: 1,
       childCount: 0,
       infantCount: 0,
@@ -57,8 +60,8 @@ class App extends React.Component {
     let { price } = this.state;
     let month, day;
     if (date !== '') {
-      month = parseInt(date.slice(0,2));
-      day = parseInt(date.slice(3,5));
+      month = parseInt(date.slice(0, 2));
+      day = parseInt(date.slice(3, 5));
       price = calendar[month][day]['cost'];
     }
     this.setState({
@@ -69,25 +72,34 @@ class App extends React.Component {
 
   handleEndChange(date) {
     const { calendar, startDate } = this.state;
-    let { price } = this.state;
+    let { totalPrice, stayDuration, price, stayCal } = this.state;
     let month, day, startMonth, startDay;
     if (date !== '') {
-      price = 0;
+      totalPrice = 0;
       const startMoment = Moment(startDate, "MM/DD/YYYY");
       const endMoment = Moment(date, "MM/DD/YYYY").add(1, 'days');
       let currMonth, currDate;
       while (startMoment.isBefore(endMoment)) {
         currMonth = startMoment.month();
         currDate = startMoment.date();
-        price += calendar[currMonth][currDate]['cost'];
+        totalPrice += calendar[currMonth][currDate]['cost'];
+        stayDuration += 1;
+        stayCal.push([startMoment, calendar[currMonth][currDate]['cost']]);
         startMoment.add(1, 'days');
       }
+      price = parseInt(totalPrice / stayDuration);
     } else if (date === '') {
       price = calendar['0']['1'].cost;
+      stayDuration = 0;
+      stayCal = [];
     }
+    console.log(stayCal)
     this.setState({
-      endDate: date,
       price: price,
+      endDate: date,
+      totalPrice: totalPrice,
+      stayDuration: stayDuration,
+      stayCal: stayCal,
     })
   }
 
@@ -98,24 +110,24 @@ class App extends React.Component {
       if (inc === 1) {
         this.setState({
           adultCount: adultCount + 1,
-          guestCountMultiplier: guestCountMultiplier*1.05,
+          guestCountMultiplier: guestCountMultiplier * 1.05,
         });
       } else {
         this.setState({
           adultCount: adultCount - 1,
-          guestCountMultiplier: guestCountMultiplier/1.05,
+          guestCountMultiplier: guestCountMultiplier / 1.05,
         });
       }
     } else if (type === 'child') {
       if (inc === 1) {
         this.setState({
           childCount: childCount + 1,
-          guestCountMultiplier: guestCountMultiplier*1.05,
+          guestCountMultiplier: guestCountMultiplier * 1.05,
         });
       } else {
         this.setState({
           childCount: childCount - 1,
-          guestCountMultiplier: guestCountMultiplier/1.05,
+          guestCountMultiplier: guestCountMultiplier / 1.05,
         });
       }
     } else if (type === 'infant') {
@@ -140,6 +152,34 @@ class App extends React.Component {
   }
 
   renderCosts() {
+    const { endDate, totalPrice, stayDuration, price, guestCountMultiplier } = this.state;
+    const finalPrice = parseInt(price * guestCountMultiplier);
+    const subCost = finalPrice * stayDuration;
+    const cleaningFee = parseInt(subCost / 8.2) + 8;
+    const serviceFee = parseInt(subCost / 10.5) + 5;
+    const nightText = stayDuration === 1 ? ' night' : ' nights';
+    const totalCost = subCost + cleaningFee + serviceFee;
+    if (endDate) {
+      return (
+        <>
+          <div className={styles.costDrop}>
+            <div>You won't be charged yet</div>
+          </div>
+            <div className={styles.costBox}>
+              <div className={styles.costLeft}>{'$'}{finalPrice}{' x '}{stayDuration}{nightText}</div><div className={styles.costRight}>{'$'}{subCost}</div>
+            </div>
+            <div className={styles.costBox}>
+              <div className={styles.costLeft}>Cleaning fee</div><div className={styles.costRight}>{'$'}{cleaningFee}</div>
+            </div>
+            <div className={styles.costBoxLast}>
+              <div className={styles.costLeft}>Service Fee</div><div className={styles.costRight}>{'$'}{serviceFee}</div>
+            </div>
+          <div className={styles.costTotal}>
+            <div className={styles.costTotalLeft}>Total</div><div className={styles.costTotalRight}>{'$'}{totalCost}</div>
+          </div>
+        </>
+      )
+    }
 
   }
 
@@ -148,18 +188,18 @@ class App extends React.Component {
       reviewCount, capacity, calendar, startDate, endDate, adultCount, childCount, infantCount, guestCountMultiplier,
     } = this.state;
     let { price } = this.state;
-    price = parseInt((price*guestCountMultiplier).toFixed(0));
+    price = parseInt(price * guestCountMultiplier);
     let { rating } = this.state;
     rating = rating.toFixed(2);
     return (
       <>
-      <div className={styles.root}>
-        <Header price={price} rating={rating} reviewCount={reviewCount} />
-        <br />
-        <Calendar capacity={capacity} calendar={calendar} handleStartChange={this.handleStartChange} handleEndChange={this.handleEndChange} startDate={startDate} endDate={endDate} adultCount={adultCount} childCount={childCount} infantCount={infantCount} handleGuestChange={this.handleGuestChange} />
-        {this.renderAvailButton()}
-        {this.renderCosts()}
-      </div>
+        <div className={styles.root}>
+          <Header price={price} rating={rating} reviewCount={reviewCount} />
+          <br />
+          <Calendar capacity={capacity} calendar={calendar} handleStartChange={this.handleStartChange} handleEndChange={this.handleEndChange} startDate={startDate} endDate={endDate} adultCount={adultCount} childCount={childCount} infantCount={infantCount} handleGuestChange={this.handleGuestChange} />
+          {this.renderAvailButton()}
+          {this.renderCosts()}
+        </div>
       </>
     );
   }
